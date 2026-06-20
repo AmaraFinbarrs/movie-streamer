08b0aa2504c8875162f37cd1091c31ae88c24bfd
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2026-06-20 02:31:03 +0100

    Create initial hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import './App.css'
+// import React from 'react'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      Hello World
-    </>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Find <span className="text-gradient">Movies</span>  You'll Enjoy Without the Hassle</h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
   )
 }
 
