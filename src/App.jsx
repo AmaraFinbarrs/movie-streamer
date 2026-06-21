17e1f52fd2d7cf2228d729fcf74072ace04ea7b4
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2026-06-21 02:44:51 +0100

    Setup appwrite to save most searched movies

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useDebounce } from 'react-use';
 import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -48,8 +49,12 @@ const App = () => {
       }
 
       const data = await response.json();
-      setMovieList(data.results || [])
+      setMovieList(data.results || []);
 
+      if(query && data.results.length > 0) {
+        updateSearchCount( query, data.results[0]);
+      }
+      
     } catch (error) {
       console.log(`Error fetching movies: ${error}`);
       setErrorMessage('Error fetching movies. Please try again later.');
